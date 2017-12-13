0e2f27e417b210ef8adc54bc3c7e0172be275644
danilo <danilovteodoro@gmail.com>
2017-12-13 14:01:46 -0200

    Updates about primitives tuyes

--- 2 Working with Java Data Types/2 Primitive Data Types/Main.java
+++ 2 Working with Java Data Types/2 Primitive Data Types/Main.java
@@ -27,16 +27,45 @@
 
     *double - 64-bit IEEE 754. Utilizado para armazenar valores de ponto flutuante. É o valor padrão de
                                 números decímais utilizado pela linguagem. Assim como o float, não é recomendado
-                                 ser utilizados par válores exatos como moéda. Utilize java.math.BigDecimal.
+                                ser utilizados par válores exatos como moéda. Utilize java.math.BigDecimal.
    
     *boolean - representa um valor boleano sendo true ou false.
    
     *char - 16-bit Unicode character. It has a minimum value of '\u0000' (or 0) and a maximum value of '\uffff' (or 65,535 inclusive).
+
+    Os campos declarados não são obrigatórios serem inicializados, caso não inicializados, o compilador irá atribuir um valor padrão.
+    os valores padrão são: 0 para tipos inteiros, 0.0 para decimais, false para boolean, null para objetos, e '\u0000' para char.
+  
+    Essa regra não se aplica a variáveis locais, devendo ser inicializadas antes de usá-las, caso contrario irá
+    ocorrer um erro de compilação.
+
+    Literais.
     
+    São chamdos de litérias os tipos que são atribuídos literalmente no código pelo programador,
+    sem a necessidade de usar u new.
+    
+    Literais Inteiros
+    os valores de byte, short e int podem ser atribuído de um valor inteiro.
+    o long é usados para receber valores inteiros longos que ultrapassam o range do int.
+    por padrão a jvm trata um número literal inteiro int. Ex:
+     20 + 20 .A JVM irá considerar uma soma de dois números inteiros int.
+    para informar que o numero é long, usa-se a letra L depis do número.
+    Pode usar a letra minúscula támbem, embora seja recomendado user maiúscula.
+    Ex: 10L
+   
+    Bases Númericas.
+    Os números inteiros podem ser representados em outras bases sem ser a decimal.
+  
+    base octal: basta colocar o 0 antes do numero.
+                Os números aceitos devem estar entre 0 e 7.
+              ex: 
+                int numOctal = 0754;
 
  
 */
 class Main{
+   static int NUMERO; //Variável de classe.
+   int numero;  //Variável de instancia.
 
   public static void main(String args[]){
     byte b = -128; //Valor Mínimo de bayte
@@ -51,10 +80,33 @@ class Main{
 
     short s = 32700;
     System.out.println("Este é um short : "+s);
+    
+    int i = 10;
+    System.out.println("Numero Inteiro "+i);
+    long l = 10000000000000L; //Se retirar o L no final não compila.
+    System.out.println("Numero Long "+l);
+    System.out.println(1050);//imprime um número inteiro.
    
     char c = 'A';
     System.out.println("Valor do char : "+c);
     c = 68; //O char também é um valor numérico. Representa um caractere unicode.
     System.out.println("Valor do char : "+c);
+   
+   //As campos de instancia e de classe assumem o valor padrão
+   System.out.println("Default new Main().numero : "+new Main().numero+
+                      " Default Main.NUMERO : "+Main.NUMERO); 
+
+   //Variáveis locais não serão inicializadas com o valor padrão.
+   //int numero;
+   //System.out.println("numero : "+numero); //Erro de compilação.
+
+   int numOctal = 0754; //Número na base octal.
+   //int numOctal = 08; //A compilação falha.
+   System.out.println("Numero 0754. BASE OCTAL  "+numOctal);
+   int numHexadecimal = 0xf1; //Número na base hexadecimal.
+   System.out.println("Numero 0754. BASE HEXADECIMAL  "+numHexadecimal);
+   int numBinario = 0b0010; //Número na base binária.
+   System.out.println("Numero 0754. BASE BINÁRIA  "+numBinario);
+   
   }
 }
